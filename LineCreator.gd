ca03ad72cba6317341368a7a5d27d972f1aa5586
Michael <michael.john.davis.1@gmail.com>
2020-10-20 07:49:05 +1300

    Snap grid for line drawing
    - Adds a snap grid for line drawing

--- LineCreator.gd
+++ LineCreator.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var is_drawing: bool = false
 var point_count: int = 0
+var grid_step: int = 100
 
 func _ready():
 	$Line2D.hide()
@@ -14,15 +15,23 @@ func _input(event):
 		if !is_drawing:
 			$Line2D.show()
 			is_drawing = true
-			_add_point_at_position(event.position)
+			_add_point_at_position(round_and_return_mouse_pos())
 			
-		_add_point_at_position(event.position)
+		_add_point_at_position(round_and_return_mouse_pos())
 
 func _process(_delta):
 	if is_drawing:
-		$Line2D.points[point_count - 1] = get_global_mouse_position()
-		print(point_count)
+		$Line2D.points[point_count - 1] = round_and_return_mouse_pos()
+		
 
 func _add_point_at_position(pos):
 	$Line2D.add_point(pos)
 	point_count += 1
+
+func round_and_return_mouse_pos():
+	#get mouse position
+	var mouse_position: Vector2 = get_global_mouse_position()
+	#set to the grid
+	mouse_position.x = stepify(mouse_position.x, grid_step)
+	mouse_position.y = stepify(mouse_position.y, grid_step)
+	return mouse_position
